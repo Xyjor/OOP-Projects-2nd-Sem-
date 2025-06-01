8179b603b7e4800da478f80accededf961343d9b
Rojyx <rojpogz09@gmail.com>
2025-06-01 17:51:52 +0800

    Add files via upload

---
+++ ImplementingSearchinStoreApp/src/ImplementingSearchInStoreAppMain.java
@@ -0,0 +1,23 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Xyjor
+ */
+public class ImplementingSearchInStoreAppMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new StoreApp().setVisible(true);
+        JOptionPane.showMessageDialog(null, "When searching for items, make sure to search\nthe product's name.");
+    }
+    
+}
